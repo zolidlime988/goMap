d4059a73d1edde4af5fbb6c93c4c06432ef3af9b
Apiwat Supaksirakol <api.supak@gmail.com>
2023-01-07 17:36:58 +0700

    example of create map with multiple ways

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	// var colors map[string]string
+	colors := make(map[string]string)
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
 	fmt.Println(colors)
 }
