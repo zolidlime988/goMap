f6648e487229c9ba9defa2105587ad5eeb222bbd
Apiwat Supaksirakol <api.supak@gmail.com>
2023-01-07 17:39:37 +0700

    how to delete map

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
 	// var colors map[string]string
 	colors := make(map[string]string)
+	colors["white"] = "#fffff"
 	// colors := map[string]string{
 	// 	"red":   "#ff0000",
 	// 	"green": "#4bf745",
 	// }
+	delete(colors, "white")
 	fmt.Println(colors)
 }
